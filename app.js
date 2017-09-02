bd4f01c11c8a33b6cb6b490a7322866597ced9c3
Akash Adhikari <akash_532998@yahoo.com>
2017-09-02 22:01:41 +0545

    Hogan template engine

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  path = require('path')
 
-
-//routes with get method and a function with three parameters
+// set the views. __dirname is the root folder and appending views folder
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.route('/').get(function(req, res, next){
-    res.send('<h1>Test</h1>');
+  res.render('index', {});
 })
+
 
 app.listen(3000, function(){
   console.log('Working on port 3000');

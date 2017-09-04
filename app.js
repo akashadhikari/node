cb0fa80d2ca089f28946654984df130ce0506b9a
Akash Adhikari <akash_532998@yahoo.com>
2017-09-04 21:41:48 +0545

    Session management

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express'),
   app = express(),
   path = require('path')
+  cookieParser = require('cookie-parser')
+  session = require('express-session')
 
 // set the views. __dirname is the root folder and appending views folder
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser()); // session management
 
+// new Express session version require: saveUninitialized:true, resave:true
+app.use(session({secret:'catscanfly'}));
 // call routes.js module
 require('./routes/routes.js')(express,app);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
+// Use Express router
+// function(express,app) suggests the use of express and app.js
 module.exports = function(express, app) {
   var router = express.Router();
 
+  // use this instance of router to define all routes
   router.get('/', function(req, res, next){
     res.render('index', {title: 'SkyChat'});
   })
@@ -9,6 +12,19 @@ module.exports = function(express, app) {
     res.render('chatrooms', {title: 'Chatrooms'});
   })
 
+  // create a session variable named favcolor and set the value red
+  router.get('/setcolor', function(req, res, next){
+    req.session.favColor = "Red";
+    res.send('Setting favourite color!');
+  })
+
+  router.get('/getcolor', function(req, res, next){
+    res.send('Fav color ' + (req.session.favColor===undefined?"Not Found":req.session.favColor));
+  })
+
+  // route to be handled by the instance of the router
   app.use('/', router);
 
 }
+
+// mongodb://akash1:akash1@ds125774.mlab.com:25774/skychat

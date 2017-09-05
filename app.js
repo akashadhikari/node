2e622d5f4cb6468d17f79e47dfb560bac6b6d301
Akash Adhikari <akash_532998@yahoo.com>
2017-09-05 21:20:16 +0545

    configurations

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express'),
   app = express(),
-  path = require('path')
-  cookieParser = require('cookie-parser')
-  session = require('express-session')
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  config = require('./config/config.js'),
+  ConnectMongo = require('connect-mongo')(session)
 
 // set the views. __dirname is the root folder and appending views folder
 app.set('views', path.join(__dirname, 'views'));
@@ -11,12 +13,27 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser()); // session management
 
-// new Express session version require: saveUninitialized:true, resave:true
-app.use(session({secret:'catscanfly'}));
+//development mode
+var env = process.env.NODE_ENV || 'development';
+if(env === 'development') {
+  // dev specific settings
+  app.use(session({secret:config.sessionSecret}))
+} else {
+  // production specific
+  app.use(session({
+    secret:config.sessionSecret,
+    store:new ConnectMongo({
+      url:config.dbURL,
+      stringify:true
+    })
+  }))
+}
+
 // call routes.js module
 require('./routes/routes.js')(express,app);
 
 
 app.listen(3000, function(){
-  console.log('Working on port 3000');
+  console.log('SkyChat working on port 3000');
+  console.log('Node: ' +env);
 })
